8488a35b20835f831682ce5c201a00e9b2e418d5
Xi Yang <xiyang@es.net>
2020-08-28 10:11:42 -0400

    Added test/test_api.py

---
+++ test/test_api.py
@@ -0,0 +1,56 @@
+import requests,json, getpass
+from yaml import load as yload
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+output = {}
+filename = '/etc/sense-o-auth.yaml' 
+with open(filename, 'r') as fd:
+    output = yload(fd.read())
+
+# https://hostname:8543/auth/realms/StackV/protocol/openid-connect/token
+token_url = output['AUTH_ENDPOINT']
+# https://hostname:8443/StackV-web/restapi
+api_url = output['API_ENDPOINT']
+RO_user = output['USERNAME']
+RO_password = output['PASSWORD']
+
+client_id = output['CLIEND_ID']
+client_secret = output['SECRET']
+
+data = {'grant_type': 'password','username': RO_user, 'password': RO_password}
+#data = {'grant_type': 'client_credentials'}
+
+print token_url
+
+access_token_response = requests.post(token_url, data=data, verify=False, allow_redirects=False, auth=(client_id, client_secret))
+print data, client_id, client_secret
+tokens = json.loads(access_token_response.text)
+#print tokens
+print '-'*100
+## Step C - now we can use the access_token to make as many calls as we want.
+#
+
+test_api_url = '%s/service/ready' % (api_url)
+api_call_headers = {'Authorization': 'Bearer ' + tokens['access_token']}
+api_call_response = requests.get(test_api_url, headers=api_call_headers, verify=False)
+print 'SENSE-O is %sready' % ('' if api_call_response.text == 'true' else 'NOT ')
+if api_call_response.text == 'false':
+    exit()
+
+
+"""
+API TEST CASES  by https://github.com/sdn-sense/sense-o-py-client/issues/1
+"""
+
+print "\n# Testing Method #1\n"
+
+for endpoint in ['/sense/discovery/edgepoints/urn:ogf:network:ultralight.org:2013']:
+    test_api_url = '%s/%s' % (api_url, endpoint)
+    api_call_headers = {'Authorization': 'Bearer ' + tokens['access_token']}
+    api_call_response = requests.get(test_api_url, headers=api_call_headers, verify=False)
+    print "Request: " + test_api_url
+    print "Response:"
+    print api_call_response.text
+    print '='*100
