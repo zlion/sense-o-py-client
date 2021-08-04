1b20736b2c6b2af6568b617bb88bdd974f5fa377
Xi Yang <xiyang@es.net>
2021-08-04 11:41:28 -0400

    source clean up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 from sense import __VERSION__
 
-with open("requirements.txt", "r") as fh:
+with open("/Users/xiyang/Work/sense-o-py-client/requirements.txt", "r") as fh:
   requirements = fh.read()
 
 setuptools.setup(
